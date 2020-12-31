ac9c5c7811f9bbdd05ea5a7feb7d1e30c8374a8e
ericgaspar <junk.eg@free.fr>
2020-12-31 19:02:43 +0100

    fix

--- scripts/_common.sh
+++ scripts/_common.sh
@@ -9,7 +9,7 @@ YNH_PHP_VERSION="7.3"
 # dependencies used by the app
 pkg_dependencies=""
 
-extra_php_dependencies="php${YNH_PHP_VERSION}-mysql php${YNH_PHP_VERSION}-pdo"
+extra_php_dependencies="php${YNH_PHP_VERSION}-mysql php${YNH_PHP_VERSION}-pdo php${YNH_PHP_VERSION}-sqlite3"
 
 #=================================================
 # PERSONAL HELPERS
