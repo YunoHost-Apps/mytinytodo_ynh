ea2ed2de434e375a330700d2c097f3045d30ea9d
Éric Gaspar <46165813+ericgaspar@users.noreply.github.com>
2021-06-06 09:07:27 +0200

    Set rights (#17)
    * Set rights

--- scripts/_common.sh
+++ scripts/_common.sh
@@ -6,9 +6,6 @@
 
 YNH_PHP_VERSION="7.3"
 
-# dependencies used by the app
-pkg_dependencies=""
-
 extra_php_dependencies="php${YNH_PHP_VERSION}-mysql php${YNH_PHP_VERSION}-pdo"
 
 #=================================================
