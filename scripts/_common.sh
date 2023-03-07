74ca8f269f5becf90dcf7dbd98afca43b0e21df5
Éric Gaspar <46165813+ericgaspar@users.noreply.github.com>
2023-03-07 22:34:15 +0100

    Version 2 (#47)
    * v2
    
    * Auto-update README
    
    * v2
    
    * Update restore
    
    ---------
    
    Co-authored-by: yunohost-bot <yunohost@yunohost.org>

--- scripts/_common.sh
+++ scripts/_common.sh
@@ -4,10 +4,6 @@
 # COMMON VARIABLES
 #=================================================
 
-YNH_PHP_VERSION="8.0"
-
-pkg_dependencies="php${YNH_PHP_VERSION}-mysqli php${YNH_PHP_VERSION}-pdo"
-
 #=================================================
 # PERSONAL HELPERS
 #=================================================
